c14607cdcd24d3509a9e4d7e0ed94d0b57cb1ac0
Christian Gonzalez <christiang333@gmail.com>
2023-04-26 21:16:22 -0500

    Finished Prduct

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
 const textareaEL = document.querySelector('.textarea');
+const characterNumbersEl = document.querySelector('.stat__number--characters');
+const twitterNumbersEl = document.querySelector('.stat__number--twitter');
+const facebookNumbersEl = document.querySelector('.stat__number--facebook');
+const wordsNumbersEl = document.querySelector('.stat__number--words');
 
-textareaEL.addEventListener('input', function(){
+const inputHandler = () => {
+    //input validation
+    if (textareaEL.value.includes('Narmeen Gonzalez')) {
+        alert('I love you!!!!!! I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!I love you!!!!!!');
+    }
+
+
     // determine new numbers
+let numberOfwords = textareaEL.value.split(' ').length;
+if (textareaEL.value.length === 0){
+    numberOfwords = 0;
+}
+const numberOfletters = textareaEL.value.length;
+const twitterChrctsLeft = 280 - numberOfletters;
+const facebookChrctsLeft = 2200 - numberOfletters;
 
+    //add Visial indicator if limit is exceeded
+    if (twitterChrctsLeft < 0 ) {
+         twitterNumbersEl.classList.add('stat__number--limit')
+    } else {
+        twitterNumbersEl.classList.remove('stat__number--limit')
+    }
+    if (facebookChrctsLeft < 0 ) {
+        facebookNumbersEl.classList.add('stat__number--limit')
+    }else {
+        facebookNumbersEl.classList.remove('stat__number--limit')
+
+    }
     //set new numbers
-})
+    characterNumbersEl.textContent = numberOfletters;
+    twitterNumbersEl.textContent=twitterChrctsLeft;
+    facebookNumbersEl.textContent = facebookChrctsLeft;
+    wordsNumbersEl.textContent = numberOfwords
+};
+
+textareaEL.addEventListener('input', inputHandler);
+
